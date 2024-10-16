c855dd9aa9dae94edbc3ef1d77177d5e570d79f3
Joshua <joshuaoghomwen35@gmail.com>
2024-10-16 11:27:14 +0100

    Added Login.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Homepge from './Componets/Homepage'
+import LoginPage from './Componets/LOGIN/LoginPage'
+import Animations from './Componets/Animations'
+import Newpage from './Newpage'
+
+function App() {
+  return (
+    <div>
+      <LoginPage /> 
+      {/* <Homepge /> */}
+      {/* <Animations/> */}
+      {/* <Newpage/> */}
+    </div>
+  )
+}
+
+export default App
